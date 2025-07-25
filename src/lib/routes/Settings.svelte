5ada425418b13ae1348ffbde91ae1f94d5071436
Ryan Hockett <ryanhockett.dev@gmail.com>
2025-07-25 01:52:58 -0500

    Added color theme selection to settings menu

--- src/lib/routes/Settings.svelte
+++ src/lib/routes/Settings.svelte
@@ -7,7 +7,6 @@
     import { push } from 'svelte-spa-router';
     import { Accordion, Tooltip } from "@skeletonlabs/skeleton-svelte";
     import  { PencilAlt, X, Eye, EyeOff }  from 'svelte-heros'
-    import { tick } from "svelte";
 
     if (!get(currentUser)){
         push('/');
@@ -26,11 +25,9 @@
     }
 
     let themeForm = {
-        theme: "",
+        theme: $theme,
     }
 
-    let Msg="filler"
-    let MsgStatus="surface-50"
 
     function toast(msg:string, stat:string) {
         const toaster = document.getElementById("toaster")
@@ -81,9 +78,9 @@
 
     async function updateTheme(){
         const user = $currentUser?.id
-        if(user && nameForm.name != ""){
+        if(user && $theme){
             await pb.collection('users').update(user, themeForm )
-            themeForm.theme = ""
+            toast("Theme saved!", "bg-success-500")
         }  
     }
 
@@ -129,7 +126,7 @@
 
     // Class variables for cleaner DOM readability
     const iconClass = "m-1 focus:outline-none focus:size-7 focus:shadow-[10px_10px_5px_1px_rgba(0,0,0,0.25)] focus:shadow-primary-700/90 transition duration-200 ease-in-out focus:inset-shadow-sm focus:inset-shadow-primary-600/20 hover:size-7 hover:shadow-[10px_10px_5px_1px_rgba(0,0,0,0.25)] hover:shadow-primary-700/90 hover:inset-shadow-sm hover:inset-shadow-primary-600/90"
-    const inputClass = "mb-3 bg-white focus:outline-none focus:shadow-[10px_10px_4px_1px_rgba(0,0,0,0.25)] focus:shadow-primary-800/30 text-surface-900 text-base rounded-lg block w-full p-1.5 focus:p-2.5 dark:focus:shadow-primary-950/40 transition duration-200 ease-in-out"
+    const inputClass = "mb-3 bg-white focus:outline-none focus:shadow-[10px_10px_4px_1px_rgba(0,0,0,0.25)] focus:shadow-primary-800/30 text-surface-900 text-base rounded-lg block w-[60%] p-1.5 focus:p-2.5 dark:focus:shadow-primary-950/40 transition duration-200 ease-in-out"
     const buttonClass = "w-[50%] text-sm focus:outline-none focus:text-lg focus:shadow-[10px_10px_5px_1px_rgba(0,0,0,0.25)] focus:shadow-primary-700/90 transition duration-200 ease-in-out focus:inset-shadow-sm focus:inset-shadow-primary-600/20 hover:text-lg hover:shadow-[10px_10px_5px_1px_rgba(0,0,0,0.25)] hover:shadow-primary-700/90 hover:inset-shadow-sm hover:inset-shadow-primary-600/90"
     const formClass = "flex flex-col p-2 items-center shadow-[10px_10px_5px_1px_rgba(0,0,0,0.25)] shadow-primary-700/90 inset-shadow-sm inset-shadow-primary-600/60"
 </script>
@@ -165,13 +162,13 @@
         </Accordion.Item>
         
         
-        <hr class="hr" />
+        <hr class="hr my-2" />
             <div class="flex justify-between px-4 py-2">
                 <h1>Email:</h1>
                 <h1>{$currentUser?.email}</h1>
                 <div></div>
             </div>
-        <hr class="hr" />
+        <hr class="hr my-2" />
         
         
         <Accordion.Item value="password" controlBase="flex w-full justify-between items-center" indicatorBase="flex items-center justify-center">
@@ -194,15 +191,10 @@
                             >
                             {#snippet trigger()}
                                 {#if type === "password"}
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 mx-2 mt-1 child">
-                                        <path fill-rule="evenodd" d="M3.28 2.22a.75.75 0 0 0-1.06 1.06l14.5 14.5a.75.75 0 1 0 1.06-1.06l-1.745-1.745a10.029 10.029 0 0 0 3.3-4.38 1.651 1.651 0 0 0 0-1.185A10.004 10.004 0 0 0 9.999 3a9.956 9.956 0 0 0-4.744 1.194L3.28 2.22ZM7.752 6.69l1.092 1.092a2.5 2.5 0 0 1 3.374 3.373l1.091 1.092a4 4 0 0 0-5.557-5.557Z" clip-rule="evenodd" />
-                                        <path d="m10.748 13.93 2.523 2.523a9.987 9.987 0 0 1-3.27.547c-4.258 0-7.894-2.66-9.337-6.41a1.651 1.651 0 0 1 0-1.186A10.007 10.007 0 0 1 2.839 6.02L6.07 9.252a4 4 0 0 0 4.678 4.678Z" />
-                                    </svg>
+                                <EyeOff class="{iconClass} size-5 hover:size-6 focus:size-6"/>
+                                    
                                 {:else}
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 mx-2 mt-1 child">
-                                        <path d="M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                                        <path fill-rule="evenodd" d="M.664 10.59a1.651 1.651 0 0 1 0-1.186A10.004 10.004 0 0 1 10 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0 1 10 17c-4.257 0-7.893-2.66-9.336-6.41ZM14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" clip-rule="evenodd" />
-                                    </svg>
+                                    <Eye class="{iconClass} size-5 hover:size-6 focus:size-6"/>
                                 {/if}
                             {/snippet}
                             {#snippet content()}Click to show/hide password{/snippet}
@@ -224,7 +216,27 @@
                 </form>
             {/snippet}
         </Accordion.Item>
-       
+        
+        <hr class="hr my-2" />
+        
+        <Accordion.Item value="theme" controlBase="flex w-full justify-between items-center" indicatorBase="flex items-center justify-center">
+            {#snippet lead()}Color theme:{/snippet}
+            {#snippet control()}{$theme}{/snippet}
+            {#snippet panel()}
+                <form on:submit|preventDefault={updateTheme} class="{formClass} pl-6">
+                    <label for="theme" class="text-white block mb-2 text-sm font-medium dark:text-white">Select a theme</label>
+                    <select bind:value={$theme} id="theme" class="{inputClass} text-center" required >
+                        <option value="wintry">Wintry</option>
+                        <option value="mona">Mona</option>
+                        <option value="pine">Pine</option>
+                        <option value="crimson">Crimson</option>
+                        <option value="mint">Mint</option>
+                        <option value="legacy">Legacy</option>
+                    </select>
+                    <button type="submit" class={buttonClass}>Save theme</button>
+                </form>
+            {/snippet}
+        </Accordion.Item>
         </Accordion>
         
         
